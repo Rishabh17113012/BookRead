4bdc59e57899dd464d6e2c0e0c809d842aaeec17
Rishabh Dev Mani Tripathi <rishabhtripathi736@gmail.com>
2025-02-09 04:41:27 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,28 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    mimeTypes: {
+      'application/javascript': ['.mjs']  
+    }
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" ? componentTagger() : null,
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",  
+      },
     },
   },
 }));
